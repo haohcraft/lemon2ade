ee1cffe867801dd54949283cda2db4f7c41ada23
Hao Huang <haohcraft@gmail.com>
2014-09-03 08:05:50 -0400

    Create a component to make the div area adjustable

---
+++ src/scripts/components/lib/Adjustable.jsx
@@ -0,0 +1,22 @@
+/**
+ * @jsx React.DOM
+ */
+
+/**
+ * To adjust the size of the area
+ */
+'use strict';
+
+var emptyFunction = require('react/lib/emptyFunction');
+
+var Adjustable = React.createClass({
+
+	render: function() {
+		return (
+			<div />
+		);
+	}
+
+});
+
+module.exports = Adjustable;
